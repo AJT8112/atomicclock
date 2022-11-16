d21b192c3be9df289db46d1e251f28e05f93a9fc
Chinyere Okolo <Chichi_Okolo@waters.com>
2022-11-16 16:04:46 +0000

    Change: useState and UseEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import { calculateNewValue } from '@testing-library/user-event/dist/utils';
+import { useState, useEffect } from 'react';
 
 const url = "https://menacingly-magnificent-spider-cloud.wayscript.cloud/"
 
-const time = fetch(url)
-  .then((response) => response.json())
-  .then((data) => data.time);
+// const time = fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => data.time);
 
 function App() {
+
+  const [time, setTime] = useState("loading time")
+
+  useEffect(() => {
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) =>setTime(data.time));
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src ="https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
         <p>
           The time is:
           {time}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
